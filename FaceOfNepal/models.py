280235ef7909450bc08b62c6f16026fb35618e37
SagarShresthaNP03A180082 <np03a180082@heraldcollege.edu.np>
2020-01-11 13:06:13 +0545

    Create frealancer

--- FaceOfNepal/models.py
+++ FaceOfNepal/models.py
@@ -1,24 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Address(models.Model):
-    title = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.title
-
-
 
 class Freelancer(models.Model):
-    Name=models.CharField(max_length=50)
-    Address=models.ForeignKey(Address,on_delete=models.CASCADE)
-    Phone=models.IntegerField()
-    Email=models.EmailField()
-    describe=models.TextField(default='Write your self')
+    Name=models.CharField(max_length=50,default='Enter your name')
+    Address=models.CharField(max_length=50,default='Enter your Address')
+    Phone=models.IntegerField(default='Enter your Number')
+    Email=models.EmailField(default='Enter your email')
+    Description=models.TextField(default='Write your self')
 
 
-    #def __self__(self):
-      #  return f'My name is {self.Name}. I am from {self.Address}. my phone number and email are {self.Phone} and {self.Email}. {self.describe}'
+    def __self__(self):
+        return f'My name is {self.Name}. I am from {self.Address}. my phone number and email are {self.Phone} and {self.Email}. {self.Description}'
         
 
 
