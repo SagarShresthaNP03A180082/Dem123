dc8e7b3ac8d41fba1ab0bb87bd52562ac6801f9b
SagarShresthaNP03A180082 <np03a180082@heraldcollege.edu.np>
2020-01-11 12:36:44 +0545

    fouth commit

--- FaceOfNepal/models.py
+++ FaceOfNepal/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+
 class Freelancer(models.Model):
     Name=models.CharField(max_length=50)
-    Address=models.CharField(max_length=100)
+    Address=models.ForeignKey(Address,on_delete=models.CASCADE)
     Phone=models.IntegerField()
     Email=models.EmailField()
     describe=models.TextField(default='Write your self')
 
 
-    def __self__(self):
-        return f'My name is {self.Name}. I am from {self.Address}. my phone number and email are {self.Phone} and {self.Email}. {self.describe}'
+    #def __self__(self):
+      #  return f'My name is {self.Name}. I am from {self.Address}. my phone number and email are {self.Phone} and {self.Email}. {self.describe}'
         
+
+
+
+
+
+  
+    
